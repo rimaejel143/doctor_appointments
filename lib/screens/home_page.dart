e972b4cc27b39e5d5bbc0142bc41855d274aec8b
Rima Ejel <ejelrima83@gmail.com>
2025-11-14 14:13:10 +0200

    Added main.dart setup and created Home Page UI

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Doctor Appointments'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/book');
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blueAccent,
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            textStyle: const TextStyle(fontSize: 18),
+          ),
+          child: const Text(
+            'Book an Appointment',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
