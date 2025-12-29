25a28c355a4b79908a1121aa226a004dbf99e047
Rayan Tayba <ghinatayba18@gmail.com>
2025-12-29 20:08:04 +0200

    add Container

--- frontend/lib/screens/confirmation_page.dart
+++ frontend/lib/screens/confirmation_page.dart
@@ -5,6 +5,40 @@ class ConfirmationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      backgroundColor: const Color(0xFFF1F9F8),
+      appBar: AppBar(
+      backgroundColor: const Color(0xFF00897B),
+        title: const Text(
+          "Appointment Confirmed",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+
+      ),
+      body: Padding(padding:  const EdgeInsets.all(25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 40),
+
+          Container(
+            padding: const EdgeInsets.all(25),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color(0xFF00897B).withOpacity(0.15),
+              ),
+               child: const Icon(
+                Icons.check_circle,
+                color: Color(0xFF00897B),
+                size: 90,
+              ),
+
+
+          )
+        ],
+      ),
+    ),
+    );
   }
 }
