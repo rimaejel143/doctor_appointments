3180a6b4ffcd7a4b1cc10e12cb7314db3fd99d1a
Rayan Tayba <ghinatayba18@gmail.com>
2025-12-29 19:16:29 +0200

    add dr info

--- frontend/lib/screens/book_appointment_page.dart
+++ frontend/lib/screens/book_appointment_page.dart
@@ -21,6 +21,60 @@ class _BookAppointmentPageState extends State<BookAppointmentPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+     final args = ModalRoute.of(context)!.settings.arguments as Map;
+
+    final int doctorId = args["doctor_id"];
+    final String doctorName = args["doctor_name"];
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF1F9F8),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFF1F9F8),
+        title: const Text("Book Appointment",
+        style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+      ),
+      body: Padding(padding: const EdgeInsets.all(20),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(14),
+                boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 8,
+                ),
+                ],
+              ),
+              child: Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 35,
+                      backgroundColor: Color(0xFF00897B),
+                      child: Icon(Icons.person,
+                          color: Colors.white, size: 35),
+                    ),
+                    const SizedBox(width: 15),
+                    Text(
+                      doctorName,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+
+            )
+          ],
+        ),
+      ),
+      ),
+    );
   }
 }
