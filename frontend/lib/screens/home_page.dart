8aa52cd01b654bf6499ea5fe60087ae1fe65144b
Rayan Tayba <ghinatayba18@gmail.com>
2025-12-29 16:31:53 +0200

    add url & scaffold

--- frontend/lib/screens/home_page.dart
+++ frontend/lib/screens/home_page.dart
@@ -14,8 +14,50 @@ class _HomePageState extends State<HomePage> {
   List<dynamic> doctors = [];
   bool isLoading = true;
   String searchQuery="";
+
+  final String apiUrl = "http://localhost:3000/doctors";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDoctors();
+  }
+   
+     Future<void> fetchDoctors() async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          doctors = data["data"];
+          isLoading = false;
+        });
+      } else {
+        throw Exception("Failed to load doctors");
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFF1F9F8),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFF1F9F8),
+        title: const Text("Doctor Apoointments"),
+        style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+        ),
+      ),
+    );
   }
 }
